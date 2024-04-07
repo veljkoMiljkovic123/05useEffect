64a9cbf80fd54d83b65e866415151915910cbeb5
unknown <veljkomiljkovicposao@gmail.com>
2024-04-07 10:46:55 +0200

    change person on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useEffect,useState } from "react"
-import CardComponent from "./components/CardComponent";
-import LoadingComponent from "./components/LoadingComponent";
+import { useEffect, useState } from "react"
+import SingleUser from "./components/SingleUser";
+
 
 function App() {
-const [myData,setMyData] = useState([])
-const[isLoading, setIsLoading] = useState(false)
 
-//drugi hook useEfect 
+  const [person,setPerson] = useState({      name:"Tihomir",
+  age:22,
+  address:"Rasovaca",
+});
 
-useEffect(()=>{
-  fetch('https://dummyjson.com/products')
-  .then(res => res.json())
-  .then(data=>{
-    setMyData(data.products)
-    setIsLoading(true)
-  })
-  .catch(err=>console.log(err))
   
-},[]);
 
-  return <div className="container mx-auto ">
-   <h1 className="text-4xl font-bold uppercase p-5 text-center bg-blue-500">Hello ReactJs</h1>
 
-  {isLoading?<div className="flex flex-wrap gap-5 "> 
-     {myData.map((el,i)=>{
-      return <CardComponent key={i} item={el} />
-     })}</div>:<LoadingComponent />}
-      
-    </div>
+  return <div className="container mx-auto">
+    <SingleUser person={person} setPerson={setPerson}/>
+
+  </div>
+   
   
 }
 
